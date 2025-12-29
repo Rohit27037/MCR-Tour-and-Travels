d74824fab07a206ef043e6cc1c6c0be5b4510027
Rohit Mishra <rohit27037@gmail.com>
2025-12-30 01:28:45 +0530

    production

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: base './' makes built assets use relative paths which works on GitHub Pages
 export default defineConfig({
 plugins: [react()],
-base: '/MCR-Tour-and-Travels/'
+base: './'
 })
